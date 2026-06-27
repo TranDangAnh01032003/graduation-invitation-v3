a27e0813b09c75b709897d287ff145e538356a4c
Tran Dang Anh <anh330701@gmail.com>
2026-06-27 07:22:00 +0700

    update: improve invitation website

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 window.INVITATION_CONFIG = {
   pageText: {
     quote: "“Hi vọng trong bức tranh thanh xuân của tớ sẽ có sự góp mặt của cậu.”",
-    introMessage: "Sau những tháng ngày cố gắng, mình rất vui khi được mời bạn đến tham dự lễ tốt nghiệp của mình.",
-    inviteLine: "Cô chú, anh chị, bạn bè tới dự lễ tốt nghiệp",
-    dateNote: "Lễ tốt nghiệp được tổ chức<br>vào lúc 10:00 - 12:00",
     school: "Trường Đại học Kinh tế - Đại học Quốc gia Hà Nội",
     hall: "Hội trường MMH",
     address: "Số 57 Phạm Hùng, Nam Từ Liêm, Hà Nội",
-    timelineNote: "Sự hiện diện của bạn là niềm hạnh phúc rất đặc biệt trong ngày đánh dấu một cột mốc mới của Quỳnh.",
-    albumTextA: "Tốt nghiệp đại học, một cánh cửa mới mở ra. Tôi đã sẵn sàng đón những cơ hội và thử thách mới.",
-    albumTextB: "Tôi từng nghĩ tốt nghiệp còn xa, nhưng rồi khoảnh khắc này cũng đến. Có lẽ chia tay mới khiến ta biết trân trọng.",
     formThanks: "Cảm ơn bạn rất nhiều vì đã gửi những lời chúc mừng tốt đẹp nhất đến buổi lễ tốt nghiệp của mình."
   }
 };
